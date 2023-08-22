6936305c197e62548e6ae333a80dfac03276ed58
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-08-21 20:21:26 -0500

    Minor comment removal

--- src/main/java/com/mybank/dto/SaleDTO.java
+++ src/main/java/com/mybank/dto/SaleDTO.java
@@ -9,5 +9,4 @@ public class SaleDTO {
     private double amount;
     private FinancialProductDTO product;
 
-    // Constructors, getters, and setters
 }
--- src/main/java/com/mybank/entities/Customer.java
+++ src/main/java/com/mybank/entities/Customer.java
@@ -34,7 +34,6 @@ public class Customer {
     private String cellPhone;
 
     public Customer() {
-        // Default constructor
     }
 
 	public Long getId() {
