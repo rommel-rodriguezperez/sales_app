37e10295fdf8010ca2c4c287c5825dcd57f05285
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-09-12 13:01:31 -0500

    Minor update to test queries

--- test_queries.sql
+++ test_queries.sql
@@ -25,3 +25,11 @@ SELECT *
 FROM Customers c
 INNER JOIN Persons p on (c.person_id = p.id);
 
+SELECT *
+FROM Employees e
+INNER JOIN Persons p on (e.person_id = p.id)
+INNER JOIN maindb.Roles r on (e.role_id = r.id)
+INNER JOIN maindb.Sales s on (s.seller_id = e.id)
+LEFT OUTER JOIN maindb.MonthlyGoals mg on (mg.employee_id = e.id)
+WHERE r.name = 'SELLER';
+
