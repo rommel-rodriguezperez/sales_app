04c32e444f1cb238397e542d742c3bd990561c3a
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-10-08 17:03:57 -0500

    The /me endpoint was not returning the right HTTP Error codes
    Now, at the very least, returns 401 UNAUTHORIZED if the token is not
    set.

--- src/main/java/com/mybank/controllers/IdentityController.java
+++ src/main/java/com/mybank/controllers/IdentityController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -69,7 +70,7 @@ public class IdentityController {
         final String jwt;
         final String username;
         if (StringUtils.isEmpty(authHeader) || !StringUtils.startsWith(authHeader, "Bearer ")) {
-            return null;
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
         jwt = authHeader.substring(7);
         username = jwtService.extractUserName(jwt);
