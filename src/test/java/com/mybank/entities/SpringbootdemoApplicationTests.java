a6582d0faccd364d144d38203ee74335eb81ce4b
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-10-25 14:09:28 -0500

    Fixed SpringBoot missing test configuration
    The Spring Boot app is already set to autoconfigure many features, but
    tetst code needs to find the application file at the root of the whole
    project (com.mybank.SpringbootdemoApplication.java). The problem was
    that I hadn't renamed the package for the test class (only test class),
    and if not found in the current package, it traverses upwards and tries
    to find it, but, because the test code was under com.demo and the
    application file was under com.mybank, it was never going to find it.
    That is why Maven was throwing errors.

--- src/test/java/com/mybank/entities/SpringbootdemoApplicationTests.java
+++ src/test/java/com/mybank/entities/SpringbootdemoApplicationTests.java
@@ -1,13 +1,16 @@
-package com.example.demo;
+package com.mybank.entities;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringR
+//import org.springframework.test.context.junit.jupiter.web.Spring
 
 @SpringBootTest
 class SpringbootdemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		return;
 	}
 
 }
