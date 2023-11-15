d0b2c5470f7dbe9d6b0066c351ae53fc77575e2d
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-11-14 20:36:58 -0500

    Small readability improvement

--- src/main/java/com/mybank/services/impl/JwtServiceImpl.java
+++ src/main/java/com/mybank/services/impl/JwtServiceImpl.java
@@ -46,7 +46,9 @@ public class JwtServiceImpl implements JwtService {
     }
 
     private String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return Jwts.builder().setClaims(extraClaims).setSubject(userDetails.getUsername())
+        return Jwts.builder()
+        		.setClaims(extraClaims)
+        		.setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 1000))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256).compact();
